4554c92aaeeab92727ddec61ba9674dac2c9a319
Utkarsh Sharma <utkarshsharma@Utkarshs-Air.lan>
2024-10-23 01:21:23 -0400

    Setting up the stage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
 import Image from "next/image";
+import Header from "@/Components/Header";
+import Navbar from "@/Components/Navbar";
+import Body from "@/Components/Body";
+import Footer from "@/Components/Footer";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Heylooo
-      
+    <div className=" items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)] bg-gradient-to-r from-slate-500 to-slate-800">
+      <div>
+        <Header/>
+      </div>
+      <div>
+        <Navbar/>
+      </div>
+      <div>
+        <Body/>
+      </div>
+      <div>
+        <Footer/>
+      </div>
     </div>
   );
 }
