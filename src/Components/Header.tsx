baeb67a78a858215b7f9c0fbd906d61167c4c7d7
Utkarsh Sharma <utkarshsharma@Utkarshs-Air.lan>
2024-10-28 01:59:49 -0400

    Setting up home page

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,5 +1,18 @@
+import Container from 'react-bootstrap/Container';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 export default function(){
     return (<>
-    This is header
+    <Container>
+        <Row>
+            <Col>This is header</Col>
+            <Col> Questions</Col>
+            <Col> Achievements</Col>
+            <Col> Leaderboard</Col>
+        </Row>
+    </Container>
+    
     </>);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,19 @@
 import Image from "next/image";
 import Header from "@/Components/Header";
-import Navbar from "@/Components/Navbar";
 import Body from "@/Components/Body";
 import Footer from "@/Components/Footer";
 
-export default function Home() {
+export default function App() {
   return (
-    <div className=" items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)] bg-gradient-to-r from-slate-500 to-slate-800">
-      <div>
-        <Header/>
-      </div>
-      <div>
-        <Navbar/>
-      </div>
-      <div>
-        <Body/>
-      </div>
-      <div>
-        <Footer/>
-      </div>
+    <div className="Background">
+      <div className="App" >
+        <div>
+          <Body/>
+        </div>
+        <div>
+          <Footer/>
+        </div>
+      </div> 
     </div>
   );
 }
